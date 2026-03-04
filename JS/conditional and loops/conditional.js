4f039275328941d4f6a5fe4468a9dc7a3a0acb8d
Ankit-Mahata <ankitmahata44@gmail.com>
2026-03-04 16:52:36 +0545

    conditional statements

---
+++ JS/conditional and loops/conditional.js
@@ -0,0 +1,19 @@
+let age=20;
+if(age>=18){
+    console.log("you can vote");
+}
+age=16;
+if(age<18){
+    console.log("you can't vote");
+}
+
+
+let mode="dark";
+let color;
+if(mode==="dark"){
+    color="black";
+}
+if(mode==="light"){
+    color="white";
+}
+console.log(color);
