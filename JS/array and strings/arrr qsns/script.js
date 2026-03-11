4e680cea24c6257c9fa31ec4dce61b612c482c0c
Ankit-Mahata <ankitmahata44@gmail.com>
2026-03-11 20:47:39 +0545

    avg marks and discoount price

--- JS/array and strings/arrr qsns/script.js
+++ JS/array and strings/arrr qsns/script.js
@@ -6,3 +6,12 @@ for(let i=0; i<stdmarks.length; i++){
     sum=sum+stdmarks[i];
 }
 console.log(`the average marks is ${sum/stdmarks.length}`);
+
+
+/*for a given arrray with price of 5 items. change the array to stor the price with 10% discount */
+let price=[250,645,300,900,50];
+console.log(`the price of items are = ${price}`);
+for(let i=0; i<price.length; i++){
+    price[i]=price[i]-price[i]*0.1;
+} 
+console.log(`the price of items after discount is = ${price}`);
