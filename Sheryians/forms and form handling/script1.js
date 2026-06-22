c6d8f8bf7cd458637e2aea2ad605076caac7b58d
Ankit-Mahata <ankitmahata44@gmail.com>
2026-06-22 18:50:22 +0545

    validator

--- Sheryians/forms and form handling/script1.js
+++ Sheryians/forms and form handling/script1.js
@@ -4,6 +4,11 @@ let form=document.querySelector("form");
 
 form.addEventListener("submit",function(dets){
     dets.preventDefault();
+    let isvalid=true;
+    
+    document.querySelector("#emailMessage").textContent=" ";
+    document.querySelector("#passwordMessage").textContent=" ";
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
@@ -11,9 +16,16 @@ form.addEventListener("submit",function(dets){
     let passans=passwordRegex.test(password.value);
 
     if(!emailans){
-     document.querySelector("#emailMessage").textContent="Email is incorrect !";  }
+     document.querySelector("#emailMessage").textContent="Invalid Email !";
+    isvalid=false;  }
      if(!passans){ 
-     document.querySelector("#passwordMessage").textContent="password is incorrect !";
+     document.querySelector("#passwordMessage").textContent="Invalid Password !";
+     isvalid=false;
      }
-    
+    if(isvalid){
+        document.querySelector("#resultMsg").textContent="Everything is correct";
+    }
+    else{
+        document.querySelector("#resultMsg").textContent="There is error";
+    }
 })
