1d7e737159c0e6e6e8f40064fdabe56cb870ec63
Ankit-Mahata <ankitmahata44@gmail.com>
2026-06-24 12:04:21 +0545

    download bar

---
+++ Sheryians/Timer and Interval/download bar/script.js
@@ -0,0 +1,27 @@
+let count=0;
+let progress=document.querySelector(".progress");
+let  button=document.querySelector("button");
+let  p=document.querySelector("p");
+let  h2=document.querySelector("h2");
+let audio=document.querySelector("audio");
+
+button.addEventListener("click",function(){
+count=0;
+progress.style.width=`0%`;
+    p.textContent="0%";
+    h2.textContent="Downloading File...";
+let cls=setInterval(function(){
+if(count<100){
+    count++;
+    progress.style.width=`${count}%`;
+    p.textContent=count+"%";
+}
+else{
+    h2.textContent="Downloaded";
+    audio.style.display="initial";
+    audio.play();
+    clearInterval(cls);
+
+}
+},30)
+}); 
