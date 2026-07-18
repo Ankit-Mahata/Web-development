71bb46e71a1ce6a80755a1a415fcb09fc01525d4
Ankit-Mahata <ankitmahata44@gmail.com>
2026-07-18 20:40:51 +0545

    callback and hell

---
+++ Sheryians/async js/script.js
@@ -0,0 +1,104 @@
+// asynchronous behaviour
+console.log("hey1");
+console.log("hey2");
+setTimeout(()=>{
+console.log("hey3");},3000);
+console.log("hey4");
+
+// callback
+
+let time;
+function wait (fnc){
+    time=Math.floor(Math.random()*10);
+    setTimeout(()=>{
+    console.log("after",time,"seconds");
+    },time*1000)
+}
+wait();
+
+//
+function rukja(fnc){
+    setTimeout(fnc,time*1000);
+}
+rukja(function(){
+    console.log("callback");
+});
+
+// 
+function greet(callback){
+    console.log("namaste");
+    callback();
+}
+function bye(){console.log("bye");};
+greet(bye);
+/* callbacks vaneko tyo function ho jaslai as
+an argument pathainchha kunai function ma , so that teslai kehi time pachhi use/call garna sakiyos */
+// here bye is a callback
+// but , yesto greet(bye()); yesto vaidiyeko vaye
+// tyo callback hune thiyena , kinaki yo bela 
+// bye() instant call hune thiyo
+/* callback ko main purpose vaneko 
+kunai kaam sakiye pachhi particular function lai execute garnu ho , khas ma function lai variable ma assign garnu ko purpose pani yehi ho  */
+// in general , kunai specific work sakiye pachhi execute hune function callback ho 
+// for eg , EventListner pani callback ho
+
+function process(callback){
+    console.log("processing");
+    setTimeout(callback,2000);
+}
+process(()=>{console.log("Email sent")});
+process(()=>{console.log("SMS sent")});
+
+// With the use of callback , a function can preform multiple task
+
+/*Callback भनेको एउटा function हो, जसलाई अर्को function मा argument को रूपमा पठाइन्छ र आवश्यक समयमा त्यसले call (execute) गर्छ। */
+
+
+
+// callback hell
+
+function login(emailpass,ab){
+    console.log("logging in...");
+    setTimeout(()=>{
+        console.log(`logged in as ${emailpass.email}`);
+        ab("welcome to the profile",emailpass);
+    },2000);
+}
+function getprofile(username,cb){
+    setTimeout(()=>{
+    console.log(`Profile fetched of ${username}`);
+    cb({username,age:20});
+    },3000)
+}
+
+function getpost(fe){
+    setTimeout(()=>{
+        console.log("getting posts");
+        fe({posts:5,like:100,comment:50});
+    },4000);
+}
+
+
+login({email:"ankit123@gmail.com",pass:"ankit111"} , function(text,emailpass){
+    console.log(text);
+
+    getprofile("Ankit",(pfdata)=>{
+    USER={...emailpass,...pfdata};
+    console.log(USER);
+
+    getpost((posts)=>{
+        USER={...USER, ...posts};
+        console.log(USER);
+    })
+});
+
+
+});
+
+
+// ek kaam sakiye pachhi arko kam garnu , tyo pani callback vitra callback lagayera , tyo lai callback hell vaninchha
+// yeslai overcome promises ra asyncwait le garchha
+
+
+
+
